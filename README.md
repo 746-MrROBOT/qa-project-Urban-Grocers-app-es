2440755c2d829cf915d738abbd17a2f551455c62
746-MrROBOT <capitandelnavio6@gmail.com>
2025-01-26 11:51:02 -0600

    Subiendo mi proyecto finalizado

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# Proyecto Urban Grocers 
+# urban grocers app 
+ este proyecto es una aplicacion diseñada para realizar pruebas de calidad y asegurar el correcto funcionamiento de "urban grocers".
+
+## Descripción proposito del proyecto
+fue desarrollado para automatizar las pruebas de un API, asegurando la calidad y seguridad de sus endpoint
+
+### Archivos y directorios Principales
+- * .idea **: archivos de configuracion del IDE
+- * .pytest_cache **: cache generada por pytest
+- * .venv **: Entorno virtual de pyton 
+- * configuration.py **: archivo que gestiona configuraciones 
+- * create_kit_test.py **: Script para ejecutar pruebs
+- * data.py **; archivo encargado de manejar los datos del proyecto
+- * sender_stand_request.py **; archivo que gestiona solicitudes
+- * __pycache__ **; Archivos cmpilados de pyton
+
+#### tecnologias utilizadas
+- lenguaje de programacion: Python
+- framework de pruebas: Pytest
+- biblioteca de python para realizar solicitudes: requests
+- Control de versiones: Git y Github
+
+##### problema que resuelve 
+-Realizar pruebas manuales para verificar el funcionamiento de una API puede ser tedioso, propenso a errores y difícil de replicar. Este proyecto automatiza ese proceso, permitiendo:  
+- Validar de manera eficiente los endpoints clave de la API.  
+- Reducir el tiempo invertido en pruebas repetitivas.  
+- Garantizar la calidad del software mediante validaciones consistentes y reproducibles
---
+++ data.py
@@ -0,0 +1,39 @@
+headers = {
+    "Content-Type": "application/json"
+}
+
+user_body = {
+    "firstName": "Andrea",
+    "phone": "+11234567890",
+    "address": "123 Elm Street, Hilltop"
+}
+
+kit_body = {
+    "name": "mi kit"
+}
+
+test1_kit_name = "a"
+test2_kit_name = "Abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd"\
+                 "abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd"\
+                 "abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd"\
+                 "abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd"\
+                 "abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd"\
+                 "abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd"\
+                 "abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd"\
+                 "abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd"\
+                 "cdabcdabcdabcdabcdabcdabcdabC"
+test3_kit_name = ""
+test4_kit_name = "Abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd"\
+                 "Abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd"\
+                 "Abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd"\
+                 "Abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd"\
+                 "Abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd"\
+                 "Abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd"\
+                 "Abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd"\
+                 "Abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd"\
+                 "cdabcdabcdabcdabcdabcdabcdabC"
+test5_kit_name = "#$%&"
+test6_kit_name = "A Aa"
+test7_kit_name = "123"
+test8_kit_name = {}
+test9_kit_name = 123
